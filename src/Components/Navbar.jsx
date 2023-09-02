97822945fe4bbd6241cfa5ebbae6c92c3a30ac2a
Hridoy-shill <ridoyaaa60@gmail.com>
2023-09-02 16:59:52 +0600

    set nevbar max width

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import { FaPaperPlane } from 'react-icons/fa';
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 max-w-7xl mx-auto">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
